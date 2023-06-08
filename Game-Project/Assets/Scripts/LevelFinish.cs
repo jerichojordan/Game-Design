daf55e01158a911806bc8e776c8697c35aa0fc0e
jerichojordan <jericho.jordan.ong@gmail.com>
2023-06-08 10:49:04 +0200

    Pause + Chair collider Fixed, Game Over bug

--- Game-Project/Assets/Scripts/LevelFinish.cs
+++ Game-Project/Assets/Scripts/LevelFinish.cs
@@ -6,11 +6,16 @@ public class LevelFinish : MonoBehaviour
 {
     [SerializeField] private GameObject GameOverUI;
     [SerializeField] private GameObject levelCompleteUI;
+    [SerializeField] GameObject Campos;
+    private GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
         GameOverUI.SetActive(false);
         levelCompleteUI.SetActive(false);
+        Campos.SetActive(true);
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -20,15 +25,19 @@ public class LevelFinish : MonoBehaviour
     }
     public void GameOver()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
         AudioListener.pause = true;
         GameOverUI.SetActive(true);
+        Campos.SetActive(false);
+        Player.GetComponent<Player>().enabled = false;
     }
 
     public void LevelComplete()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
         AudioListener.pause = true;
         levelCompleteUI.SetActive(true);
+        Campos.SetActive(false);
+        Player.GetComponent<Player>().enabled = false;
     }
 }
--- Game-Project/Assets/Scripts/PauseMenu.cs
+++ Game-Project/Assets/Scripts/PauseMenu.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,11 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject PauseMenuUI;
     public bool isPaused;
     private GameObject Player;
-    private GameObject Camera;
+    [SerializeField] GameObject Campos;
     // Update is called once per frame
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-        Camera = GameObject.FindGameObjectWithTag("MainCamera");
         isPaused = false;
         DeactivateMenu();
 
@@ -35,21 +35,21 @@ public class PauseMenu : MonoBehaviour
     }
     public void ActivateMenu()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
         AudioListener.pause = true;
         PauseMenuUI.SetActive(true);
         Player.GetComponent<Player>().enabled = false;
-        Camera.GetComponent<CameraTargetting>().enabled = false;
+        Campos.SetActive(false);
     }
 
     public void DeactivateMenu()
     {
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
         AudioListener.pause = false;
         PauseMenuUI.SetActive(false);
         isPaused = false;
         Player.GetComponent<Player>().enabled = true;
-        Camera.GetComponent<CameraTargetting>().enabled = true;
+        Campos.SetActive(true);
     }
 
     public void MainMenu()
