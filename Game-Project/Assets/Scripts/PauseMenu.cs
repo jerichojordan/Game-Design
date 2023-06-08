62a2ab4041e8525f551df04e42a40e75696c2b8a
fp5774s <fachrialdimaspp15@gmail.com>
2023-06-09 00:53:35 +0200

    ingame Volume Setting

--- Game-Project/Assets/Scripts/PauseMenu.cs
+++ Game-Project/Assets/Scripts/PauseMenu.cs
@@ -1,40 +1,50 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject PauseMenuUI;
+    [SerializeField] GameObject Campos;
+    [SerializeField] private GameObject SettingUI;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Text VolumeText;
+
+
     public bool isPaused;
     private GameObject Player;
-    [SerializeField] GameObject Campos;
     // Update is called once per frame
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         isPaused = false;
         DeactivateMenu();
+        LoadValues();
+        SettingUI.SetActive(false);
 
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) )
         {
-            isPaused = !isPaused;
-        }
-        if (isPaused)
-        {
-            ActivateMenu();
-        }
-        else
-        {
-            DeactivateMenu();
+            if (!isPaused)
+            {
+                ActivateMenu();
+            }
+            else
+            {
+                DeactivateMenu();
+                SettingUI.SetActive(false);
+            }
         }
     }
     public void ActivateMenu()
     {
+        isPaused = true;
         Time.timeScale = 0f;
         AudioListener.pause = true;
         PauseMenuUI.SetActive(true);
@@ -56,5 +66,33 @@ public class PauseMenu : MonoBehaviour
     {
         DeactivateMenu();
         SceneManager.LoadScene(0);
+    }
+    public void VolumeSlider(float volume)
+    {
+        VolumeText.text = volume.ToString("0.0");
+        volumeSlider.value = volume;
+    }
+    public void saveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+        DeactivateSetting();
+    }
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+    public void ActivateSetting()
+    {
+        PauseMenuUI.SetActive(false);
+        SettingUI.SetActive(true);
+    }
+    public void DeactivateSetting()
+    {
+        PauseMenuUI.SetActive(true);
+        SettingUI.SetActive(false);
     }
 }
