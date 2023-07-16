c93ad344216fc0e4fb125fddf4f27b6c338691b9
jerichojordan <jericho.jordan.ong@gmail.com>
2023-07-16 16:53:37 +0200

    Fix Boss & Boss Level color change

--- Game-Project/Assets/Scripts/UI_ManagerScript/SongTrigger.cs
+++ Game-Project/Assets/Scripts/UI_ManagerScript/SongTrigger.cs
@@ -6,19 +6,22 @@ public class SongTrigger : MonoBehaviour
 {
     [SerializeField] private AudioClip BossSong;
     private GameObject player;
+    private AudioSource audioSource;
+    private bool isPlayed;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        audioSource = this.GetComponent<AudioSource>();
+        isPlayed = false;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AudioSource.PlayClipAtPoint(BossSong, player.transform.position);
+        if (!isPlayed)
+        {
+            audioSource.clip = BossSong;
+            isPlayed = true;
+            audioSource.Play();
+        }
+        
     }
 }
